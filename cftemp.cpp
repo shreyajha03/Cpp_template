e2599c337b1ca069c550ea4f40173c71be5f4df8
Shreya Jha <98100329+shreyajha03@users.noreply.github.com>
2023-08-27 14:38:16 +0530

    Create cftemp.cpp

---
+++ cftemp.cpp
@@ -0,0 +1,31 @@
+// SHREYA JHA
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ld long double
+#define pb push_back
+#define endl '\n'
+void printBinary(int num)
+{
+    for (int i = 10; i >= 0; i--)
+    {
+        cout << ((num >> i) & 1);
+    }
+    cout << endl;
+}
+void solve(int n)
+{
+    return;
+}
+int32_t main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        solve(n);
+    }
+    return 0;
+}
