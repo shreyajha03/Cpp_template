996d5d17c030c9264c30b1292d951a5d2806462f
Shreya Jha <98100329+shreyajha03@users.noreply.github.com>
2024-01-04 22:24:05 +0530

    Update cftemp.cpp

--- cftemp.cpp
+++ cftemp.cpp
@@ -4,15 +4,18 @@ using namespace std;
 #define int long long
 #define ld long double
 #define pb push_back
-#define endl '\n'
-// __builtin_popcount(int n)
-// __builtin_popcountl(long int n)
+#define endl '\n' 
+
+// right shift (>>1) - division by 2, left shift(<<1) - multiplication by 2
+
+// __builtin_popcount(int n)  // builtin func to calc set bit
+// __builtin_popcountl(long int n) // more than 31 
 // __builtin_popcountll(long long n)
 void printBinary(int num)
 {
     for (int i = 10; i >= 0; i--)
     {
-        cout << ((num >> i) & 1);
+        cout << ((num >> i) & 1);  //i th bit ko zeroth pos pe le jakar check kar rahe hai if it's 1 or 0
     }
     cout << endl;
 }
@@ -47,19 +50,61 @@ int bitCount(int a)
     }
     return cnt;
 }
-void solve(int n)
+// check odd or even (bit manipulation is faster than modulo operator)
+bool check(int i){
+    if(i&1!=0) return 0; // odd
+    else return 1;    // even
+}
+
+// check power of 2
+bool checkpow2(int n){
+    if(n&(n-1)) return 0; // not power of 2
+    else return 1;        // power of 2
+}
+int gcd(int a, int b)
 {
+    if (b == 0)
+        return a;
+    else
+        return gcd(b, a % b); // tc - O(log(min(a,b)))
+}
+
+// conversion of uppercase to lowercase
+// binary conversion of lowercase has bit next to msb is set 
+void conversion(char c){
+    // // upper to lower - set 
+    // cout<<char(c|(1<<5));
+    // // lower to upper - unset
+    // cout<<char(c&(~(1<<5)));
+    // u to l
+    char B = 'B';
+    char b = B | ' ';
+    cout<<b<<endl;
+
+    // l to u
+    char d = 'd';
+    char D = (d&'_');
+    cout<<D<<endl;
+
+    return;
+}
+
+void solve(vector<int> v, int n, int k)
+{
+  
     return;
 }
 int32_t main()
 {
     int t;
-    cin >> t;
-    while (t--)
-    {
-        int n;
-        cin >> n;
-        solve(n);
+    cin>>t;
+    while(t--){
+    int n, k;
+    cin >> n>>k;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+        cin >> v[i];
+    solve(v, n, k);
     }
     return 0;
 }
